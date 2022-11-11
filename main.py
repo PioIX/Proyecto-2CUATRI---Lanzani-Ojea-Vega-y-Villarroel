17c8f6203faed0068ce56031d8de32ff624e6e8d
Bruno Mateo Mateo <100933623+brunovillarroel@users.noreply.github.com>
2022-11-11 10:50:41 +0000

    Actualización de progreso

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 from flask import Flask, render_template, request, redirect, session, jsonify
 import sqlite3
 import json
+import datetime
 
 app = Flask(__name__)
 app.secret_key = 'heladeracaliente123'
 
+
+cantidadDeNoticias = 24
+
+def obtenerNoticias():
+  conn = sqlite3.connect('baseDeDatos.db')
+  resu = conn.execute(f"""SELECT * FROM Noticias""")
+  listaNoticias = []
+  for linea in resu:
+    listaNoticias.append(linea)
+  conn.close()
+  return listaNoticias
+
 @app.route('/')
 def index():
-    return render_template('index.html', navActive = 1)
+  return render_template('index.html', navActive = 1)
 
-@app.route('/home')
+@app.route('/home', methods=["GET", "POST"])
 def home():
   return render_template('home.html', navActive = 2)
+
+@app.route('/ajaxObtenerNoticias', methods=["GET", "POST"])
+def ajaxObtenerNoticias():
+  if request.method == "GET":
+    noticias = obtenerNoticias()
+    return json.dumps([noticias, cantidadDeNoticias, session['nombre']])
+  else:
+   return render_template("home.html", navActive = 2)   
 
 @app.route('/buscar')
 def buscar():
@@ -39,13 +60,14 @@ def verificarInicioSesion():
 
 @app.route('/registro')
 def registro():
-  return render_template("registro.html")
+  return render_template("registro.html", navActive = 0)
 
 @app.route('/verificarRegistro', methods=["GET", "POST"])
 def verificarRegistro():
   if request.method == "POST":
     nombre = request.form["nomUsuario"]
     contraseña = request.form["conUsuario"]
+    confirmacionContraseña = request.form["confirmarConUsuario"]
     carrera = request.form["carreraUsuario"]
     conn = sqlite3.connect('baseDeDatos.db')
     resu = conn.execute(f"""SELECT id FROM Usuarios WHERE nombre = '{nombre}'""")
@@ -54,12 +76,114 @@ def verificarRegistro():
       print("El nombre ya fue tomado por otro usuario")
       return redirect('/registro')
     else:
-      resu = conn.execute(f"""INSERT INTO Usuarios (nombre, contraseña, carrera) VALUES ('{nombre}', '{contraseña}', '{carrera}')""")
-      conn.commit()
-      conn.close()
-      return redirect('/')
+      if contraseña == confirmacionContraseña:
+        resu = conn.execute(f"""INSERT INTO Usuarios (nombre, contraseña, carrera) VALUES ('{nombre}', '{contraseña}', '{carrera}')""")
+        conn.commit()
+        conn.close()
+        return redirect('/')
+      else:
+        conn.commit()
+        conn.close()
   else:
     return render_template('registro.html')
+
+@app.route('/cerrarSesion')
+def cerrarSesion():
+  session['nombre'] = ""
+  return redirect('/')
+
+@app.route('/creadorDeNoticias', methods=["GET", "POST"])
+def creadorDeNoticias():
+  if session['nombre'] == "admin":
+    return render_template('creadorDeNoticias.html')
+  else:
+    return redirect('/')
+
+@app.route('/agregarNuevaNoticia', methods=["GET", "POST"])
+def agregarNuevaNoticia():
+  if request.method == "POST":
+    categoria = request.form["categoria"]
+    titulo = request.form["titulo"]
+    contenido = request.form["contenido"]
+    autor = request.form["autor"]
+    tiempoActual = datetime.datetime.now()
+    conn = sqlite3.connect('baseDeDatos.db')
+    conn.execute(f"""INSERT INTO Noticias (titulo, contenido, likes, fecha, categoria, autor) VALUES ('{titulo}', '{contenido}', 0, '{tiempoActual}', '{categoria}', '{autor}')""")
+    conn.commit()
+    conn.close()
+    global cantidadDeNoticias
+    cantidadDeNoticias += 1
+    return json.dumps(True)
+  else:
+    return render_template('creadorDeNoticias.html')
+
+@app.route('/noticia/<int:idNoticia>', methods=["GET", "POST"])
+def noticia(idNoticia):
+  for i in obtenerNoticias():
+    if i[0] == idNoticia:
+      noticia = i
+  conn = sqlite3.connect("baseDeDatos.db")
+  idUsuario = conn.execute(f"""SELECT id FROM Usuarios WHERE nombre = '{session['nombre']}'""")
+  id = 0
+  for i in idUsuario:
+    id = i[0]
+  return render_template('noticia.html', navActive = 5, noticia = noticia, idUsuario = id)
+
+@app.route('/eliminarNoticia', methods=["DELETE"])
+def eliminarNoticia():
+  if request.method== "DELETE":
+    idPregunta = request.form["idPregunta"]
+    conn = sqlite3.connect('baseDeDatos.db')
+    conn.execute(f"""DELETE FROM Noticias WHERE id = {idPregunta}""")
+    conn.commit()
+    conn.close()
+    return json.dumps(True)
+  else:
+    return redirect("/home")
+
+
+@app.route('/ajaxGestionLikes', methods=["GET", "POST", "PUT"])
+def ajaxGestionLikes():
+  if request.method == "PUT":
+    cantidadLikes = request.form["cantidadLikes"]
+    idNoticia = request.form["idNoticia"]
+    conn = sqlite3.connect('baseDeDatos.db')
+    conn.execute(f"""UPDATE Noticias SET likes = {cantidadLikes} WHERE id = {idNoticia}""")
+    conn.commit()
+    conn.close()
+    return json.dumps(True)
+  elif request.method == "POST":
+    idNoticia = request.form["idNoticia"]
+    idUsuario = request.form["idUsuario"]
+    conn = sqlite3.connect('baseDeDatos.db')
+    resu = conn.execute(f"""SELECT * FROM UsuariosLikes WHERE idUsuario = {idUsuario} AND idNoticia = {idNoticia}""")
+    if resu.fetchone():
+      conn.execute(f"""DELETE FROM UsuariosLikes WHERE idUsuario = {idUsuario} AND idNoticia = {idNoticia}""")
+      conn.commit()
+      conn.close()
+    else:
+      conn.execute(f"""INSERT INTO UsuariosLikes (idUsuario, idNoticia) VALUES ({idUsuario}, {idNoticia})""")
+      conn.commit()
+      conn.close()
+    return json.dumps(True)
+  else:
+    return render_template("noticia.html")
+
+@app.route('/verificarNoticiaLikeada', methods=["GET", "POST"])
+def verificarNoticiaLikeada():
+  if request.method == "POST":
+    idUsuario = request.form["idUsuario"]
+    idNoticia = request.form["idNoticia"]
+    conn = sqlite3.connect("baseDeDatos.db")
+    resu = conn.execute(f"""SELECT * FROM UsuariosLikes WHERE idUsuario = {idUsuario} AND idNoticia = {idNoticia}""")
+    if resu.fetchone():
+      conn.close()
+      return json.dumps(True)
+    else:
+      conn.close()
+      return json.dumps(False)
+  else:
+    return render_template("noticia.html")
 
 
 app.run(host='0.0.0.0', port=81)
--- static/script.js
+++ static/script.js
@@ -19,3 +19,226 @@ function iniciarSesion(){
     },
   });
 }
+
+function coincidirContraseñas(){
+  nombreUsuarioRegistro = document.getElementById("nombreUsuarioRegistro").value;
+  contraseñaUsuarioRegistro = document.getElementById("contraseñaUsuarioRegistro").value;
+  confirmarContraseñaUsuarioRegistro = document.getElementById("confirmarContraseñaUsuarioRegistro").value;
+  if (nombreUsuarioRegistro == "" || contraseñaUsuarioRegistro == "" || confirmarContraseñaUsuarioRegistro == ""){
+    alert("No se completaron todos los campos")
+  }
+  else{
+    if (contraseñaUsuarioRegistro == confirmarContraseñaUsuarioRegistro){
+      document.getElementById("formularioRegistro").submit();
+    }
+    else{
+      alert("Las contraseñas no coinciden");
+      document.getElementById("contraseñaUsuario").value = "";
+      document.getElementById("confirmarContraseñaUsuario").value = "";
+      document.getElementById("contraseñaUsuario").focus();
+    }
+  }
+}
+
+function obtenerNoticias(){
+  $.ajax({
+    url:"/ajaxObtenerNoticias",
+    type:"GET",
+    success: function(response){
+      noticias = JSON.parse(response);
+      for (let i = 0; i < noticias[1]; i ++){
+        if (noticias[2] == null || noticias[2] == ""){
+          document.getElementById("sectionNoticias").innerHTML += `
+            <abbr title="Iniciá sesión para leer la noticia completa" style="text-decoration:transparent">
+              <section class="noticia">
+                <div class="divTituloNoticias">
+                  <h4 class="tituloNoticias" >${noticias[0][i][1]}</h4>
+                </div>
+                <div class="divContenidoNoticias">
+                  <p class="contenidoNoticias">${noticias[0][i][2]}</p>
+                </div>
+                <div class="divAutoresNoticias">
+                  <p class="autoresNoticias">${noticias[0][i][6]}</p>
+                </div>
+              </section>
+            </abbr>
+          `;
+        }
+        else{
+          document.getElementById("sectionNoticias").innerHTML += `
+            <abbr class="etiquetaLeerNoticia" title="Leer noticia completa">
+              <a id="${noticias[0][i][0]}" class="botonDeNoticias" href="noticia/${noticias[0][i][0]}">
+                <section class="noticia">
+                  <div class="divTituloNoticias">
+                    <h4 class="tituloNoticias">${noticias[0][i][1]}</h4>
+                  </div>
+                  <div class="divContenidoNoticias">
+                    <p class="contenidoNoticias">${noticias[0][i][2]}</p>
+                  </div>
+                  <div class="divAutoresNoticias">
+                    <p class="autoresNoticias">${noticias[0][i][6]}</p>
+                  </div>
+                </section>
+              </a>
+            </abbr>
+          `;
+        }
+      }
+    },
+    error: function(error){
+      console.log(error);
+    },
+  });
+}
+
+function agregarNuevaNoticia(){
+  categoria = document.getElementById("categoria").value;
+  titulo = document.getElementById("titulo").textContent;
+  contenido = document.getElementById("contenido").textContent;
+  autor = document.getElementById("autor").value;
+  $.ajax({
+    url:"/agregarNuevaNoticia",
+    type:"POST",
+    data: {"categoria":categoria, "titulo":titulo, "contenido":contenido, "autor":autor},
+    success: function(response){
+      resultado = JSON.parse(response);
+      if (resultado == true){
+        document.getElementById("formNuevaNoticia").submit();
+      }
+    },
+    error: function(error){
+      console.log(error);
+    },
+  });
+}
+
+function seleccionarFuncionEliminar(){
+  htmlDivHome = document.getElementById("navBase").innerHTML;
+  document.getElementById("navBase").innerHTML = "";
+  htmlHomeSinModificar = document.getElementById("divHome").innerHTML;
+  document.getElementById("botonNuevaNoticia").outerHTML = "";
+  document.getElementById("botonEditarNoticia").outerHTML = "";
+  document.getElementById("botonEliminarNoticia").outerHTML = `
+    <a id="botonEliminarNoticia" onclick="cancelarEliminarNoticia()">Cancelar</a>
+  `;
+  document.getElementById("tituloAdmin").outerHTML = `
+    <h3 id="alertaEliminarNoticia">Seleccioná la noticia que quieras eliminar</h3>
+  `;
+  for (let x = 0; x < document.getElementsByClassName("botonDeNoticias").length; x ++){
+    document.getElementsByClassName("etiquetaLeerNoticia")[x].setAttribute('title', 'Hacé click para eliminar esta noticia')
+    document.getElementsByClassName("botonDeNoticias")[x].removeAttribute('href');
+    document.getElementsByClassName("botonDeNoticias")[x].setAttribute('onclick', `eliminarNoticia(${document.getElementsByClassName("botonDeNoticias")[x].getAttribute("id")})`);
+    document.getElementById("alertaEliminarNoticia").setAttribute('style', 'border: 2px solid red; color: red;');
+  }
+  document.getElementById("etiquetaStyleHome").innerHTML = `
+    .noticia:hover h4, .noticia:hover h6, .noticia:hover p{
+      color: red;
+    }
+    .noticia:hover{
+      border: 2px solid red;
+    }
+  `;
+}
+
+function cancelarEliminarNoticia(){
+  document.getElementById("navBase").innerHTML = htmlDivHome;
+  document.getElementById("divHome").innerHTML = htmlHomeSinModificar;
+  document.getElementById("etiquetaStyleHome").innerHTML = "";
+}
+
+function eliminarNoticia(idPregunta){
+  $.ajax({ 
+    url:"/eliminarNoticia", 
+    type:"DELETE", 
+    data: {"idPregunta":idPregunta},
+    success: function(response){
+      datos = JSON.parse(response);
+      location.reload();
+    },
+    error: function(error){ 
+      console.log(error);
+    }, 
+  });
+}
+
+function mostrarComentarios(){
+  if (document.getElementById("comentarios").innerHTML == ""){ 
+    document.getElementById("comentarios").innerHTML = `
+      <h4>Dejá acá tus comentarios:</h4>
+      <input type="text" placeholder="Escriba aquí..." required autofocus>
+    `;
+  }
+  else{
+    document.getElementById("comentarios").innerHTML = "";
+  }
+}
+
+function darLike(idNoticia, idUsuario){
+  document.getElementById("boxIconCorazoncito").setAttribute('type', 'solid');
+  document.getElementById("boxIconCorazoncito").setAttribute('color', 'red');
+  document.getElementById("labelLikes").innerHTML = parseInt(document.getElementById("labelLikes").innerHTML) + 1;
+  gestionLikes(parseInt(document.getElementById("labelLikes").innerHTML), idNoticia, idUsuario);
+  document.getElementById("corazoncito").setAttribute('onclick', `sacarLike(${idNoticia}, ${idUsuario})`);
+}
+
+function sacarLike(idNoticia, idUsuario){
+  document.getElementById("boxIconCorazoncito").setAttribute('type', 'regular');
+  document.getElementById("boxIconCorazoncito").setAttribute('color', 'white');
+  document.getElementById("labelLikes").innerHTML = parseInt(document.getElementById("labelLikes").innerHTML) - 1;
+  gestionLikes(parseInt(document.getElementById("labelLikes").innerHTML), idNoticia, idUsuario)
+  document.getElementById("corazoncito").setAttribute('onclick', `darLike(${idNoticia}, ${idUsuario})`);
+}
+
+function gestionLikes(cantidadLikes, idNoticia, idUsuario){
+  cantidadLikes = cantidadLikes;
+  idNoticia = idNoticia;
+  idUsuario = idUsuario;
+  $.ajax({ 
+    url:"/ajaxGestionLikes", 
+    type:"PUT", 
+    data: {"cantidadLikes":cantidadLikes, "idNoticia":idNoticia},
+    success: function(response){
+      datos = JSON.parse(response);
+    },
+    error: function(error){ 
+      console.log(error);
+    }, 
+  });
+  $.ajax({ 
+    url:"/ajaxGestionLikes", 
+    type:"POST",
+    data: {"idNoticia":idNoticia, "idUsuario":idUsuario},
+    success: function(response){
+      datos = JSON.parse(response);
+    },
+    error: function(error){ 
+      console.log(error);
+    }, 
+  });
+}
+
+function verificarNoticiaLikeada(idUsuario, idNoticia){
+  idUsuario = parseInt(idUsuario);
+  idNoticia = parseInt(idNoticia);
+  $.ajax({ 
+    url:"/verificarNoticiaLikeada", 
+    type:"POST",
+    data: {"idUsuario":idUsuario, "idNoticia":idNoticia},
+    success: function(response){
+      datos = JSON.parse(response);
+      if (datos == true){
+        document.getElementById("boxIconCorazoncito").setAttribute('type', 'solid');
+        document.getElementById("boxIconCorazoncito").setAttribute('color', 'red');
+        document.getElementById("corazoncito").setAttribute('onclick', `sacarLike(${idNoticia}, ${idUsuario})`);
+      }
+      else{
+        document.getElementById("boxIconCorazoncito").setAttribute('type', 'regular');
+        document.getElementById("boxIconCorazoncito").setAttribute('color', 'white');
+        document.getElementById("corazoncito").setAttribute('onclick', `darLike(${idNoticia}, ${idUsuario})`);
+      }
+    },
+    error: function(error){ 
+      console.log(error);
+    }, 
+  }); 
+}
